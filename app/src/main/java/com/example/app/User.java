d9d860336a97d9a04ac7c829a51583b55e1a27ee
davidg1991 <47544110+davidg1991@users.noreply.github.com>
2019-04-06 23:00:09 -0400

    Update User.java

--- app/src/main/java/com/example/app/User.java
+++ app/src/main/java/com/example/app/User.java
@@ -2,24 +2,37 @@ package com.example.app;
 
 public class User
 {
-    
-
     private String userName;
     private Integer userID;
     private long sessionID;
+    private String userRole;
 
 
-    public User(String userName, Integer userID,long sessionID)
+    public User(String userName, Integer userID,long sessionID,String userRole)
     {
         this.userName=userName;
         this.userID=userID;
         this.sessionID=sessionID;
+        this.userRole=userRole;
+    }
+
+    public User(String userName, Integer userID,long sessionID)
+    {
+        this.userName = userName;
+        this.userID = userID;
+        this.sessionID = sessionID;
     }
 
     public User(String userName,Integer userID)
     {
         this.userName=userName;
         this.userID=userID;
+    }
+
+    public User(String userName,String userRole)
+    {
+        this.userRole=userRole;
+        this.userName=userName;
     }
 
     public String getUserName()
@@ -48,4 +61,16 @@ public class User
     public void setSessionID(long sessionID) {
         this.sessionID = sessionID;
     }
+
+    public String getUserRole()
+    {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) 
+    {
+        this.userRole = userRole;
+    }
+
+
 }
