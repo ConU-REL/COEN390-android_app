60710fa9f60949627d33439819433181809048d1
davidg1991 <47544110+davidg1991@users.noreply.github.com>
2019-04-08 01:11:02 -0400

    Update UserInfosActivity.java

--- app/src/main/java/com/example/app/UserInfosActivity.java
+++ app/src/main/java/com/example/app/UserInfosActivity.java
@@ -1,6 +1,7 @@
 package com.example.d_gille.manageusers;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,25 +11,46 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.List;
 
 public class UserInfosActivity extends AppCompatActivity
 {
     private static final String TAG="ManageUsersActivity";
+
+    DatabaseHelper dbHelper=new DatabaseHelper(this);
+    int userID;
+    String name;
+    String role;
+    long session_id;
+    SharedPreferencesHelper sharedPreferencesHelper;
+    DatabaseHelper databaseHelper;
+    User user;
+    List<User> userList;
+    int position;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_infos);
+        final Bundle bundle=getIntent().getExtras();
+        userID=bundle.getInt("usersID");
+        role=bundle.getString("userRole");
+        session_id=bundle.getLong("session_id");
+        name=bundle.getString("userName");
+        user=new User(name,role,userID,session_id);
+        sharedPreferencesHelper=new SharedPreferencesHelper(this);
+        databaseHelper=new DatabaseHelper(this);
 
-        //goToManageUsersActivity();
-        //customDialog("Dialog Box","this is the dialog box","cancelMethod","okMethod");
+
         AlertDialog.Builder userBuilder=new AlertDialog.Builder(UserInfosActivity.this);
         View view=getLayoutInflater().inflate(R.layout.dialog_manage,null);
         EditText message=(EditText) view.findViewById(R.id.messageEditText);
         Button deleteUserButton=(Button) view.findViewById(R.id.deleteUserButton);
         Button changeRoleButton=(Button) view.findViewById(R.id.changeUserRoleButton);
-        int id;
+
         TextView userInfoTextView=(TextView) view.findViewById(R.id.userInfoTextView);
 
         deleteUserButton.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +58,7 @@ public class UserInfosActivity extends AppCompatActivity
             public void onClick(View v)
             {
                 toastMessage("User deleted!");
-                //deleteUser(id);
+                deleteUser(userID);
 
             }
         });
@@ -45,8 +67,7 @@ public class UserInfosActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                toastMessage("Change of role successful!");
-                //changeUserRole(id);
+              updateUserRole(position);
 
             }
         });
@@ -63,6 +84,9 @@ public class UserInfosActivity extends AppCompatActivity
             {
                 Log.d(TAG,"onClick:Cancel Called:");
                 cancelMethod();
+                Intent intent=new Intent(UserInfosActivity.this,ManageUsersActivity.class);
+                startActivity(intent);
+
 
 
 
@@ -101,6 +125,33 @@ public class UserInfosActivity extends AppCompatActivity
     {
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
+
+    public void updateUserRole(int position)
+    {
+        userList=dbHelper.getAllUsers();
+        Intent intent=new Intent(UserInfosActivity.this,ChangeRoleActivity.class);
+        int userID = userList.get(position).getUserID();
+        String userRole = userList.get(position).getUserRole();
+        String userName =  userList.get(position).getUserName();
+        long session_id=userList.get(position).getSessionID();
+
+        intent.putExtra("usersID", userID);
+        intent.putExtra("userName", userName);
+        intent.putExtra("userRole", userRole);
+        intent.putExtra("session_id",session_id);
+        startActivity(intent);
+
+    }
+
+    public void deleteUser(int userID)
+    {
+        dbHelper.deleteUserbyID(userID);
+        Intent intent=new Intent(UserInfosActivity.this,ManageUsersActivity.class);
+        startActivity(intent);
+    }
+
+
+
 
 
 }
